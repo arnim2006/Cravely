3d578259710282764a56dbed3e591130f8033488
Arnim Agarwal <24ucc030@lnmiit.ac.in>
2025-12-28 03:07:33 +0530

    Initial commit: Cravely full-stack MERN project

--- Frontend/src/pages/auth/UserRegister.jsx
+++ Frontend/src/pages/auth/UserRegister.jsx
@@ -24,7 +24,7 @@ const UserRegister = () => {
             password
         },
         {
-            withCredentials: true  // for cookies
+            withCredentials: true
         })
 
         console.log(response.data);
